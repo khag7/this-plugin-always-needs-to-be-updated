1dbdfbd2f1483947c54e04263bd8ca01eaac9d61
Kevin Hagerty <kevin@khagerty.com>
2015-12-14 21:39:37 -0500

    tweaks

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
-    Plugin Name: This Plugin Always Needs To Be Updated
-	Description: When active, this plugin will always show that it has an update available, the slug for this plugin has special characters, similar to a WooThemes plugin.
+	Plugin Name: This Plugin Always Needs To Be Updated
+	Description: This will always show as an available update. Also, the slug for this plugin has special characters, similar to a WooThemes plugin.
 	Author: Kevin Hagerty
 	Author URI: https://profiles.wordpress.org/khag7/
 	Version: 1.0
@@ -11,7 +11,7 @@
 add_filter( 'site_transient_update_plugins', function( $transient ){
 	
 	$plugin_file = plugin_basename( __FILE__ );
-	$plugin_data = get_plugin_data( $plugin_file );
+	$plugin_data = get_plugin_data( trailingslashit( WP_PLUGIN_DIR ) . $plugin_file );
 	
 	$transient->response[ $plugin_file ] = (object) array(
 		'plugin'      => $plugin_file,
